7e0b41c902e758fce3685af962454a1d397c6040
Javier Calatrava <javiercalatrava@capitole-consulting.com>
2020-04-18 13:13:29 +0200

    [feature] Blank project with basic pods installed (R.Swift, Realm, Alamofire...)

---
+++ ePills/ePills/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ePills
+//
+//  Created by Javier Calatrava on 18/04/2020.
+//  Copyright © 2020 Javier Calatrava. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
